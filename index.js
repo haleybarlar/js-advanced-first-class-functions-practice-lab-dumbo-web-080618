e136e06ff50a5653f29a287d8bca412b1fae6a01
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 12:08:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-// Code your solution in this file!
-function logDriverNames(drivers) {
-  drivers.forEach(function (driver) {
-    console.log(driver.name)
+const drivers = [
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+]
+
+function logDriverNames() {
+  drivers.map(driver => console.log(driver.name))
+}
+
+function logDriversByHometown(drivers, hometown) {
+  drivers.filter(driver => {
+    if (driver.hometown === hometown) {
+      console.log(driver.name)
+    }
   })
 }
 
-function logDriversByHometown(drivers, location) {
-  drivers.filter(function (driver) {
-    // debugger
-    return driver.hometown === location
-  }).map(function (driver) {
-    console.log(driver.name)
+function driversByRevenue() {
+  return drivers.sort(function(a, b) {
+    return a.revenue - b.revenue
   })
 }
 
-function driversByRevenue(drivers) {
-  return drivers.slice().sort(function (driver1, driver2) {
-    return driver1.revenue - driver2.revenue
+function driversByName() {
+  return drivers.sort(function(a, b) {
+    let nameA = a.name.toUpperCase()
+    let nameB = b.name.toUpperCase()
+    
+    if (nameA < nameB) {
+      return -1
+    } else if (nameA > nameB) {
+      return 1
+    }
+
+    return 0
   })
-}
-
-function driversByName(drivers) {
-  return drivers.slice().sort(function (driver1, driver2) {
-    return driver1.name.charCodeAt(0) - driver2.name.charCodeAt(0)
-  })
-}
-
-function totalRevenue(drivers) {
-  return drivers.reduce(function (accumulator, currentDriver) {
-    return accumulator + currentDriver.revenue;
-  }, 0)
-}
-
-function averageRevenue(drivers) {
-  return totalRevenue(drivers) / drivers.length;
 }
